2f53c045c6038f60586502fe0b80c2c74997d8f4
STAINOH Marcelo Sebastian <ms.stainoh@eramet.com>
2025-02-28 14:28:12 -0300

    fix optimizer bug, now it accepts multiple dictionaries

--- src/optimizer.py
+++ src/optimizer.py
@@ -95,17 +95,19 @@ class Optimizer:
         
         attrs = {e: {edge_param: v} for e, v in zip(edges, values)}
         nx.set_edge_attributes(self.n.G, attrs)  
-
-    def set_get_heads(self, X: np.ndarray, test_rate: Dict[int, float],
-                      head_bc: Optional[Dict[int, float]] = None,
-                       ) -> Dict[int, float]:
-        """
-        Sets the edge parameters and computes the node heads.
-        """
+    
+    def set_X(self, X: np.ndarray) -> None:
+        """Sets the edge parameters to the values in X."""
         for i, edge_param in enumerate(self.edge_params):
             param_values = X[i * len(self.edges): (i + 1) * len(self.edges)]
             self.set_edge_params(edge_param, param_values)
-        
+
+    def get_heads(self,test_rate: Dict[int, float],
+                      head_bc: Optional[Dict[int, float]] = None,
+                       ) -> Dict[int, float]:
+        """
+        computes the node heads.
+        """
         if self.use_balance:
             _, node_heads = self.n.balance_solve(rate_bc=test_rate, head_bc=head_bc, as_dict=True)
         else:
@@ -113,34 +115,35 @@ class Optimizer:
         
         return node_heads
 
-    def get_error(self, X, test_rate, test_head, head_bc=None, output_frame=False):
+    def get_error(self, X, test_rates, test_heads, head_bc=None, output_frame=False):
         """
         Calculates the cost function.
         Inputs:
         X: values corresponding to params (in order) - see below
-        test_rate: rate dictionary (node: array)
-        test_pressure: pressure_dictionary (node: array)
+        test_rate: list of rate dictionary (node: array)
+        test_pressure: list of pressure_dictionary (node: array)
         """
-        # get propagated rates
-        node_heads = self.set_get_heads(X, test_rate, head_bc)
+        self.set_X(X)
+        errors = pd.Series()
+        print(test_rates, test_heads)
+        for test_rate, test_head in zip(test_rates, test_heads):
+            # get propagated rates
+            node_heads = self.get_heads(test_rate, head_bc)
 
-        # intersect common nodes
-        common_nodes = set(test_head.keys()).intersection(node_heads.keys())
+            # intersect common nodes
+            common_nodes = list(set(test_head.keys()).intersection(node_heads.keys()))
 
-        # calculate error
-        error = np.array([np.abs(node_heads[node] - test_head[node]) for node in common_nodes])
-
+            # calculate error
+            error = pd.Series(node_heads).loc[common_nodes] - pd.Series(test_head).loc[common_nodes]
+            errors = errors.add(error.abs(), fill_value=0)
+            
         # return (dataframe or scalar)
         if output_frame:
-            out = pd.concat(list(map(pd.Series, (node_heads, test_head, test_rate))), axis=1)
-            out.columns = ['calc_heads', 'test_heads', 'test_rates']
-            out = out.loc[list(common_nodes)]
-            out['error'] = error
-            return out
+            return errors
 
-        return self.cost_func(error)
+        return self.cost_func(errors)
     
-    def optimize(self, test_rate: Dict[int, float], test_head: Dict[int, float],
+    def optimize(self, test_rates: List[Dict[int, float]], test_heads: List[Dict[int, float]],
                  head_bc: Optional[Dict[int, float]] = None,
                  bounds: Optional[np.ndarray] = None, use_balance: bool = False) -> Optional[dict]:
         """Runs the optimization process to fit edge parameters."""
@@ -149,12 +152,12 @@ class Optimizer:
         
         try:
             result = minimize(
-                self.get_error, X0, args=(test_rate, test_head, head_bc, use_balance), 
+                self.get_error, X0, args=(test_rates, test_heads, head_bc, use_balance), 
                 bounds=bounds, **self.minimize_kwargs
             )
             if self.debug:
                 print(result)
-        except Exception as e:
+        except ValueError as e:
             warnings.warn(f"Optimization failed: {e}")
             return None
         
